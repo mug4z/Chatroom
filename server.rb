c6249f2bd78b7743c2767b8745e91e1466f5e9ba
Schneiter924 <raphael.schneiter@gmail.com>
2019-03-26 09:01:37 +0100

    entête a utiliser

---
+++ server.rb
@@ -0,0 +1,8 @@
+=begin
+Nom ......... : server.rb
+Role ........ : sert de serveur de messagerie pour la chatroom
+Auteur ...... : Raphael Schneiter
+Version ..... : V1.1 du 26/03/2019
+Licence ..... : GPL
+
+=end
