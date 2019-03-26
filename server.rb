7f1571142dff07ef27a9d384095df1bdba0591f0
FRILY Timothee <Timothee.FRILY@cpnv.ch>
2019-03-26 18:24:56 +0100

    Commit from master

--- server.rb
+++ server.rb
@@ -1,16 +1,4 @@
 require 'socket'
-<<<<<<< HEAD
-require './Chat_command.rb'
-class Server
-  def initialize(socket_address, socket_port)
-     @server_socket = TCPServer.open(socket_port, socket_address)
-     @connected_clients = Hash.new
-     puts 'Started server.........'
-     run
-  end
-   def establish_chatting(username, connection)
-      loop do
-=======
 require './functions'
 
 class Server
@@ -23,7 +11,6 @@ class Server
 
   def establish_chatting(username, connection)
     loop do
->>>>>>> master
          message = connection.gets.chomp
          bye_user(message, username)
          puts @connected_clients
@@ -32,19 +19,6 @@ class Server
          end
       end
    end
-<<<<<<< HEAD
-   chat_command
-   def bye_user(message, username)
-     if message == "/bye"
-      puts "#{username} Goodbye friend !"
-      @connected_clients[username].puts "quit"
-     end
-   end
-end
-
-Server.new(8080,"localhost")
-=======
 end
 
 Server.new("localhost",8080)
->>>>>>> master
